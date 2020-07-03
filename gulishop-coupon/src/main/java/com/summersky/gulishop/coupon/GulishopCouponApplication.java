a68d79590a8838a8cdb7e778b22194ef7ddd4df0
zengfanbin <lethe_higanbana@sina.com>
2020-07-03 17:48:05 +0800

    环境搭建-优惠券服务模块

---
+++ gulishop-coupon/src/main/java/com/summersky/gulishop/coupon/GulishopCouponApplication.java
@@ -0,0 +1,16 @@
+package com.summersky.gulishop.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lenovo
+ */
+@SpringBootApplication
+public class GulishopCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulishopCouponApplication.class, args);
+    }
+
+}
