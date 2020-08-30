9c94fea84fb1fe041c5b1f01732c306302fad849
zengfanbin <lethe_higanbana@sina.com>
2020-08-30 12:58:48 +0800

    构建聚合分析

--- gulishop-search/src/main/java/com/summersky/gulisop/search/service/impl/ShopSearchServiceImpl.java
+++ gulishop-search/src/main/java/com/summersky/gulisop/search/service/impl/ShopSearchServiceImpl.java
@@ -14,13 +14,16 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.nested.NestedAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import javax.swing.*;
 import java.io.IOException;
 
@@ -149,6 +152,36 @@ public class ShopSearchServiceImpl implements ShopSearchService {
             highlightBuilder.postTags("</b>");
             searchSourceBuilder.highlighter(highlightBuilder);
         }
+
+        /**
+         * 聚合分析
+         */
+        // 1、品牌聚合
+        TermsAggregationBuilder brand_agg = AggregationBuilders.terms("brand_agg");
+        brand_agg.field("brandId").size(50);
+        // 品牌聚合的子聚合
+        brand_agg.subAggregation(AggregationBuilders.terms("brand_name_agg").field("brandName").size(1));
+        brand_agg.subAggregation(AggregationBuilders.terms("brand_img_agg").field("brandImg").size(1));
+
+        // 2、分类聚合
+        TermsAggregationBuilder catalog_agg = AggregationBuilders.terms("catalog_agg").field("catalogId").size(20);
+        catalog_agg.subAggregation(AggregationBuilders.terms("catalog_name_agg").field("catalogName").size(1));
+        searchSourceBuilder.aggregation(catalog_agg);
+
+        // 3、属性聚合
+        NestedAggregationBuilder attr_agg = AggregationBuilders.nested("attr_agg", "attrs");
+        // 聚合出所有attrId
+        TermsAggregationBuilder attr_id_agg = AggregationBuilders.terms("attr_id_agg").field("attrs.attrId");
+        // 聚合出当前attrid对应的名字
+        attr_id_agg.subAggregation(AggregationBuilders.terms("attr_name_agg").field("attrName").size(1));
+        // 聚合分析出当前attr_id对应的所有可能的属性值attrValue
+        attr_id_agg.subAggregation(AggregationBuilders.terms("attr_value_agg").field("attrValue").size(50));
+        attr_agg.subAggregation(attr_id_agg);
+        searchSourceBuilder.aggregation(attr_agg);
+
+        String s = searchSourceBuilder.toString();
+        System.out.println("构建的DSL语句"+s);
+
         SearchRequest searchRequest = new SearchRequest(new String[]{EsConstant.PRODUCT_INDEX}, searchSourceBuilder);
         return searchRequest;
     }
